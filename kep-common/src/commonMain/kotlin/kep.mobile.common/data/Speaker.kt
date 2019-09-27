5136f5504e9bed8773f050aacb7795489e286515
Romain Boisselle <romain@boisselle.dev>
2019-09-27 11:55:42 +0200

    add common data classes for Speaker / Talk

---
+++ kep-common/src/commonMain/kotlin/kep.mobile.common/data/Speaker.kt
@@ -0,0 +1,15 @@
+package kep.mobile.common.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Speaker(
+    val id: String,
+    val name: String,
+    val company: String? = null,
+    val description: String,
+    val twitter: String? = null,
+    val webSite: String? = null,
+    val talks: List<String> = emptyList(),
+    val featured: Boolean = false
+)
